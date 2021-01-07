415c2e0aaf50c16373f7beed442949f561f0e32e
chendelin1982 <chendelin1982@126.com>
2021-01-07 11:05:35 +0800

    Update en.js

--- docs/.vuepress/sidebar/en.js
+++ docs/.vuepress/sidebar/en.js
@@ -30,7 +30,7 @@ module.exports = [
         children: [
           '/admin-services',
           '/admin-mysql',
-          ['https://support.websoft9.com/docs/linux/lang-erlang.html','Erlang'],
+          ['https://support.websoft9.com/docs/linux/webs-nginx.html','Nginx'],
           ['https://support.websoft9.com/docs/linux/admin-practices.html','Linux'],
         ]
       },
